8b4701e09f66320b1d8776146898fb3ade50333e
Fernando Mercado <fermercado@live.com>
2022-06-22 19:06:34 -0300

    preparando ambiente

---
+++ react_event_plataform/src/lib/apollo.ts
@@ -0,0 +1,6 @@
+import { ApolloClient , InMemoryCache} from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pprolz2g6u01w751nt9mm8/master',
+  cache: new InMemoryCache()
+})
