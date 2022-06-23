d5af24b10d334a11ea710124d821f55f31690bba
Fernando Mercado <fermercado@live.com>
2022-06-23 17:55:14 -0300

    replace

--- react_event_plataform/src/lib/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient , InMemoryCache} from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pprolz2g6u01w751nt9mm8/master',
-  cache: new InMemoryCache()
-})
