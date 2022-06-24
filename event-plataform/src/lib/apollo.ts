38391f372777f9ee126a9503ec36f2cc085fbb24
Fernando Mercado <fermercado@live.com>
2022-06-24 16:39:16 -0300

    upgrade apollo api/token

--- event-plataform/src/lib/apollo.ts
+++ event-plataform/src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri:'https://api-sa-east-1.graphcms.com/v2/cl4pprolz2g6u01w751nt9mm8/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache
 })
